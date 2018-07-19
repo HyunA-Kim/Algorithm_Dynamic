239ad8d705707907d56879804adf531a5fa61526
HyunA-Kim <HyunA-Kim@users.noreply.github.com>
2018-07-19 10:27:56 +0900

    첫번째 제출

--- Making1/Making1/Making1.cpp
+++ Making1/Making1/Making1.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void main() {
+int main() {
 	int num;
 	int count = 0;
 	cin >> num;
@@ -10,8 +10,10 @@ void main() {
 		if (num % 3 == 0) num = num / 3;
 		else if (num % 2 == 0) num = num / 2;
 		else num = num - 1;
+		cout << num;
 		count++;
 		if (num == 1) break;
 	}
 	cout << count;
+	return 0;
 }
