ea7ea2e913fba34071d09ceae9627afed88fa29d
HyunA-Kim <HyunA-Kim@users.noreply.github.com>
2018-08-06 20:30:46 +0900

    3

---
+++ DynamicSolution/DynamicSolution/[11722]가장 긴 감소하는 부분 수열.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+	int array[1001];
+	int N,min=0;
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		int num;
+		cin >> num;
+		array[i] = num;
+	}
+
+}
